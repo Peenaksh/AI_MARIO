1ab488b1ea177b9d058f623b0dc4f3987b12b6c3
Peenaksh <85746966+Peenaksh@users.noreply.github.com>
2023-01-17 17:33:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ function preload() {
 function setup() {
 	canvas = createCanvas(1240,336);
 	instializeInSetup(mario);
+	canvas.parent('canvas')
+	video = createCapture(VIDEO)
+	video.size(800,400)
+    video.parent('game_console')
 }
 
 function draw() {
